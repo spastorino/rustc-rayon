b7413810a4f0f7229c0738ad651726af877ef5ca
Niko Matsakis <niko@alum.mit.edu>
2019-09-12 06:29:59 -0400

    Revert "WIP enable logs always"
    This reverts commit 00b69e7a15c6676f00a80aae7e1345f2451a54de.

--- rayon-core/src/log.rs
+++ rayon-core/src/log.rs
@@ -8,9 +8,11 @@
 //! replacement of the now deprecated `RAYON_RS_LOG` environment
 //! variable, which is still supported for backwards compatibility.
 
+#[cfg(debug_assertions)]
 use std::env;
 
-#[derive(Debug)]
+#[cfg_attr(debug_assertions, derive(Debug))]
+#[cfg_attr(not(debug_assertions), allow(dead_code))]
 pub(super) enum Event {
     TickleOne {
         source_worker: usize,
@@ -127,15 +129,28 @@ pub(super) enum Event {
     },
 }
 
+#[cfg(debug_assertions)]
 lazy_static! {
     pub(super) static ref LOG_ENV: bool =
         env::var("RAYON_LOG").is_ok() || env::var("RAYON_RS_LOG").is_ok();
 }
 
+#[cfg(debug_assertions)]
 macro_rules! log {
     ($event:expr) => {
         if *$crate::log::LOG_ENV {
             eprintln!("{:?}", $event);
+        }
+    };
+}
+
+#[cfg(not(debug_assertions))]
+macro_rules! log {
+    ($event:expr) => {
+        if false {
+            // Expand `$event` so it still appears used, but without
+            // any of the formatting code to be optimized away.
+            $event;
         }
     };
 }
